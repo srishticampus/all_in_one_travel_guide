89ea287283711a32c870f43a741fc90d13f231a7
Jithin Jose <jithinjose.sics@gmail.com>
2025-05-02 10:21:15 +0530

    changed logo

--- client/src/Components/tourist/navbar/TouristNavbar.jsx
+++ client/src/Components/tourist/navbar/TouristNavbar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import appLogo from "../../../Asset/images/logo.jpg";
+import appLogo from "../../../Asset/images/alchemy.png";
 import { useNavigate } from "react-router-dom";
 
 export default function TouristNavbar() {
--- client/src/pages/agency/navbar/navbar.jsx
+++ client/src/pages/agency/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import appLogo from "../../../Asset/images/logo.jpg";
+import appLogo from "../../../Asset/images/alchemy.png";
 import "./navbar.scss";
 const AgencyNavbar = () => {
   return (
